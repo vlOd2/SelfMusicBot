b3433d1effedfe66f6340a4d0a42f059c678318f
vlOd <66838724+vlOd2@users.noreply.github.com>
2023-09-11 21:58:23 +0300

    Add check if the video is a funny 0 length one

--- Commands/Info.py
+++ Commands/Info.py
@@ -21,12 +21,13 @@ async def cmd_info(instance : SelfMusicBot,
         audio_requester = instance.get_user(audio_data['discord_user_id'])
 
         audio_duration = audio_data['duration']
-        audio_time_elapsed = int(time()) - audio_source.start_time
+        audio_time_elapsed = int(time()) - audio_source.start_time if audio_duration > 0 else 0
+        progress_bar_progress = audio_time_elapsed / audio_duration if audio_duration > 0 else 0
 
         info_msg = f":notes: Currently streaming [{audio_title}](<{audio_data['webpage_url']}>):\n"
         info_msg += f":arrow_up: Requested by {audio_requester}\n"
         info_msg += f":clock3: {Utils.get_secs_formatted(audio_time_elapsed)}/{Utils.get_secs_formatted(audio_duration)}\n"
-        info_msg += f"{Utils.get_progress_bar(audio_time_elapsed / audio_duration)} "
+        info_msg += f"{Utils.get_progress_bar(progress_bar_progress)} "
 
         await message.reply(info_msg)
     else:
